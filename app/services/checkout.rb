c0d1678d9de2da2aefa36d260af75976e2c2c7f7
OJ De Quintos <2008oliverjesus@gmail.com>
2025-08-12 17:59:41 +0800

    add checkout service

---
+++ app/services/checkout.rb
@@ -0,0 +1,37 @@
+class Checkout
+  attr_reader :rules, :scanned
+
+  def initialize(rules: [])
+    @rules = rules
+    @scanned = []
+  end
+
+  def scan(code)
+    @scanned << code
+  end
+
+  def total_cents
+    counts = scanned.group_by(&:itself).trasnform_values(&:size)
+    products = Product.where(code: code.keys).index_by(&:code)
+
+    counts.sum do |code, count|
+      product = products[code]
+      raise "Unknown product '#{code}'" unless product
+
+      rule = rules.find { |r| r.applicable_skus.include?(code) }
+      if rule
+        rule.applu(product: product, count: count)
+      else
+        product.price_cents * count
+      end
+    end
+  end
+
+  def total
+    total_cents.to_f / 100.0
+  end
+
+  def formatted_total
+    format('%.2f', total)
+  end
+end
